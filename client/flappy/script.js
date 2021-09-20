286e6fa542950a750627daa03ea51f3a5738cba4
WilliamGiovanini <wgiovani@terpmail.umd.edu>
2021-09-20 14:08:46 -0400

    Update script.js

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -27,8 +27,30 @@ document.addEventListener('DOMContentLoaded' , () => {
     document.addEventListener('keyup', control)
 
     function generateObstacle() {
+        let obstacleLeft = 500
+        let randomHeight = Math.random() * 60
+        let obstacleBottom = randomHeight
         const obstacle = document.createElement('div')
         obstacle.classList.add('obstacle')
+        gameDisplay.appendChild(obstacle)
+        obstacle.style.left = obstacleLeft + 'px'
+        obstacle.style.bottom = obstacleBottom + 'px'
+
+        function moveObstacle() {
+            obstacleLeft -= 2
+            obstacle.style.left = obstacleLeft + 'px'
+
+            if (obstacleLeft === -50) {
+                clearInterval(timerId)
+                gameDisplay.removeChild(obstacle)
+            }
+        }
+        let timerId = setInterval(moveObstacle, 20)
+
+
     }
+    generateObstacle()
+
+
 
 })
