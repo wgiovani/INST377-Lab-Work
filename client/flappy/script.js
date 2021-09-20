15df69c5c85de6a70d4b201b4f631dc23e806ad7
WilliamGiovanini <wgiovani@terpmail.umd.edu>
2021-09-20 13:47:00 -0400

    Update script.js

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -5,12 +5,23 @@ document.addEventListener('DOMContentLoaded' , () => {
 
     let birdLeft = 220
     let birdBottom = 100
+    let gravity = 2
 
     function startGame() {
+        birdBottom -= gravity
         bird.style.bottom = birdBottom + 'px'
         bird.style.left = birdLeft + 'px'
-
     }
+    let timerId = setInterval(startGame, 20)
+
+    clearInterval(timerId)
+
+    function jump() {
+        birdBottom += 50
+        bird.style.bottom = birdBottom + 'px'
+    }
+    document.addEventListener('keyup', jump)
 
 
+    
 })
