56c6526be4d7401a9a24b2555ac50d8491185296
WilliamGiovanini <wgiovani@terpmail.umd.edu>
2021-09-22 10:31:16 -0400

    Update script.js

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -2,4 +2,43 @@ let slidePosition = 0;
 const slides = document.getElementsByClassName('carousel_item');
 const totalSlides = slides.length;
 
-console.log(slides);
+document.
+    getElementById('carousel_button--next')
+    .addEventListener("click", function() {
+        moveToNextSlide();
+    })
+
+document.
+    getElementById('carousel_button--previous')
+    .addEventListener("click", function() {
+        moveToPrevSlide();
+    })
+
+function updateSlidePosition() {
+    for ( let slide of slides){
+        slide.classList.remove('carousel_item--visible');
+        slide.classList.add('carousel_item--hidden');
+    }
+
+    slides[slidePosition].classList.add('carousel_item--visible')
+}
+
+function moveToNextSlide(){
+    if (slidePosition === totalSlides - 1){
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    updateSlidePosition();
+}
+
+function moveToPrevSlide(){
+    if(slidePosition === 0){
+        slidePosition= totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+
+    updateSlidePosition();
+}
