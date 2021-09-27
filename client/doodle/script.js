9a56c02dbe5627644c58fd758d79f143f7c3dcce
WilliamGiovanini <wgiovani@terpmail.umd.edu>
2021-09-27 17:10:13 -0400

    Update script.js

--- client/doodle/script.js
+++ client/doodle/script.js
@@ -2,13 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
     let doodlerLeftSpace = 50
-    let doodlerBottomSpace = 150
+    let startPoint = 150
+    let doodlerBottomSpace = startPoint
     let isGameOver = false
     let platformCount = 5
     let platforms = []
     let upTimerId
     let downTimerId
     let isJumping = true
+    let isGoingLeft = false
+    let isGoingRight = false
+    let leftTimerId
+    let rightTimerId
+    
 
     function createDoodler() {
         grid.appendChild(doodler)
@@ -59,7 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
         upTimerId = setInterval(function () {
             doodlerBottomSpace += 20
             doodler.style.bottom = doodlerBottomSpace + 'px'
-            if (doodlerBottomSpace > 350){
+            if (doodlerBottomSpace > startPoint + 200){
                 fall()
             }
         }, 30)
@@ -82,7 +88,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     (doodlerLeftSpace <= (platform.left + 85)) &&
                     !isJumping
                 ) {
-                     jump()
+                    startPoint = doodlerBottomSpace
+                    jump()
                 }
             })
         }, 30)
@@ -96,12 +103,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function control() {
         if (e.key === "ArrowLeft") {
-
+            moveLeft()
         }else if (e.key === "ArrowRight") {
-
+            moveRight()
         }else if (e.key === "ArrowUp") {
 
         }
+    }
+
+    function moveLeft() {
+        if (isGoingRight) {
+            clearInterval(rightTimerId)
+            isGoingRight = false
+        }
+        isGoingLeft = true
+        leftTimerId = setInterval(function () {
+            if(doodlerLeftSpace >=0 ){
+                doodlerLeftSpace -=5
+                doodler.style.left = doodlerLeftSpace + 'px'
+            } else moveRight()
+            
+        }, 30)
+    }
+
+    function moveRight() {
+        if (isGoingLeft) {
+            clearInterval(leftTimerId)
+            isGoingLeft = false
+        }
+        isGoingRight = true
+        leftTimerId = setInterval(function () {
+            if (doodlerLeftSpace <= 340) {
+                doodlerLeftSpace +=5
+                doodler.style.left = doodlerLeftSpace + 'px'
+            }else moveLeft()
+
+        }, 30)
     }
 
     function start () {
@@ -110,6 +147,7 @@ document.addEventListener('DOMContentLoaded', () => {
             createDoodler()
             setInterval(movePlatforms,30)
             jump()
+            document.addEventListener('keyup',control)
         }
     }
 
