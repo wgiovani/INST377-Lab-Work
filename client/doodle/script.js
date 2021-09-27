44e5ae3fb37e59c96c243058ca2a7f14af4c1dab
WilliamGiovanini <wgiovani@terpmail.umd.edu>
2021-09-27 17:43:28 -0400

    Update script.js

--- client/doodle/script.js
+++ client/doodle/script.js
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (
                     (doodlerBottomSpace >= platform.bottom) &&
                     (doodlerBottomSpace <= platform.bottom + 15) &&
-                    ((doodlerLeftSpace + 75) >= platform.left) &&
+                    ((doodlerLeftSpace + 85) >= platform.left) &&
                     (doodlerLeftSpace <= (platform.left + 85)) &&
                     !isJumping
                 ) {
